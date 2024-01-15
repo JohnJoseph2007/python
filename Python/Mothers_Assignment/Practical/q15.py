eb37a01f2ac7bcf7714670f2352ef9bcebc56a54
John Joseph <47251026+JohnJoseph2007@users.noreply.github.com>
2024-01-16 00:21:15 +0530

    Update q15.py
    teehee smol mistake

--- Python/Mothers_Assignment/Practical/q15.py
+++ Python/Mothers_Assignment/Practical/q15.py
@@ -4,7 +4,7 @@ for i in range(n):
     eid = int(input("Enter employee id: "))
     en = input("Enter employee name: ")
     es = float(input("Enter employee salary: "))
-    d[eid]={"empname":en, "empsalary":es}
+    d[eid]= (en, es)
 
 for i in d:
-    print("Employee ID: ", i, "\nEmployee Name: ", d[i]["empname"], "\nEmployee Salary: ", d[i]['empsalary'])
+    print("\nEmployee ID: ", i, "\nEmployee Name: ", d[i][0], "\nEmployee Salary: ", d[i][1],"\n")
